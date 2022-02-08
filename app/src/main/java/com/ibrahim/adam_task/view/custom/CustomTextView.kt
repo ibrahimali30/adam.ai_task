9db826435312975d43fd559bb021be01538b2385
ibrahimali <ibrahim.ali@emiratesauction.ae>
2022-02-08 14:21:00 +0200

    download assets

--- app/src/main/java/com/ibrahim/adam_task/view/custom/CustomTextView.kt
+++ app/src/main/java/com/ibrahim/adam_task/view/custom/CustomTextView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -81,6 +82,8 @@ class CustomTextView : LinearLayout {
         }
 
         a.recycle()
+
+        Log.d("TAG", "init: $textDimension $textColor $textString $textDrawable")
 
     }
 
